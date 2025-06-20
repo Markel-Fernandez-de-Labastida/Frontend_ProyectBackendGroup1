a06ec516692dc454aebcf339f204938b809e7e72
FranciscoRomanS <frankar11294@gmail.com>
2025-06-20 11:49:51 +0200

    modif-header-addToDashboard

--- src/routes/prueba.routes.js
+++ src/routes/prueba.routes.js
@@ -1,7 +1,11 @@
-const { Router } = require(‘express’);
+const { Router } = require('express');
 const router = Router();
 router.get('/dashboard', (req, res) => {
     res.render('user/dashboard')
+})
+
+router.get('/header', (req, res) => {
+    res.render('templates/header')
 })
 
 module.exports = router
